9d1ab471841d3f5420cec514479a534f406a2e96
repl.it user <replituser@example.com>
2020-10-23 09:57:15 +0000

    Assignment1New

--- Main.java
+++ Main.java
@@ -1,22 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    zahlenfilter();
-  }
-
-  public static void zahlenfilter(){
-    int ergebnis = 0;
-
-    for (int i = 0; i < 200; i++) {
-      if (i % 5 == 0) {
-        System.out.println(i + " ist durch 5 teilbar!" + "\n");
-      }
-      if (i % 10 == 9) {
-        System.out.println(i + " endet auf 9!" + "\n");
-      }
-      if ((i + (i-1)) % 3 == 0) {
-        ergebnis = i + (i-1);
-        System.out.println(i + " und " + (i-1) + " ergeben " + ergebnis + " und " + ergebnis + " ist durch 3 teilbar!" + "\n");
-      }
-    }
+    System.out.println("Hello Felix!");
   }
 }
